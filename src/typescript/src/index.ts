3a83bfba21f44e8972b1280052fd0fb5432c057c
Davide Mendolia <davide@gokarumi.com>
2025-04-11 17:19:52 +0200

    fix: update environment variable access to use bracket notation for consistency

--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -1,11 +1,11 @@
 import { app } from './infrastructure/server';
 import { appLogger } from './utils/logger';
 
-const port = process.env.PORT || 3000;
+const port = process.env['PORT'] || 3000;
 
 app.listen(port, () => {
   appLogger.info(`Server is running on port ${port}`, {
     port,
-    env: process.env.NODE_ENV || 'development',
+    env: process.env['NODE_ENV'] || 'development',
   });
 });
