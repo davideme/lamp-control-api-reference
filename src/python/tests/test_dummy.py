aae0a8df4d37692d419078c988b4b9a7604c3e6b
Davide Mendolia <davide@gokarumi.com>
2025-05-01 12:19:24 +0200

    remove python implementation

--- src/python/tests/test_dummy.py
+++
@@ -1,6 +0,0 @@
-"""Dummy test module to verify CI pipeline functionality."""
-
-
-def test_dummy() -> None:
-    """A dummy test that always passes."""
-    assert True, "This test should always pass"
