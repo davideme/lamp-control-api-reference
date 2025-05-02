a302249d648b07cbef6215d998f558405feff5bc
Davide Mendolia <davide@gokarumi.com>
2025-05-02 20:10:43 +0200

    Add E2E coverage

--- src/typescript/jest.config.js
+++ src/typescript/jest.config.js
@@ -1,9 +1,36 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    moduleFileExtensions: ['ts', 'js'],
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
     transform: {
-        '^.+\\.ts$': 'ts-jest',
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+                tsconfig: {
+                    "forceConsistentCasingInFileNames": true,
+                    "resolveJsonModule": true,
+                    "module": "ESNext",
+                    "moduleResolution": "bundler"
+                }
+            },
+        ],
+    },
+    testMatch: ['**/test/**/*.test.ts'],
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/types/**/*.ts',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
     },
 }; 
--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -1,31 +1,11 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import fastify from 'fastify'
-import fastifyOpenapiGlue from 'fastify-openapi-glue';
-import Service from './service';
-import Security from './security';
+import { buildApp } from './app';
 
-const __filename = fileURLToPath(import.meta.url);
-const currentDir = dirname(__filename);
-
-const options = {
-    specification: `${currentDir}/../../../docs/api/openapi.yaml`,
-    service: new Service(),
-    securityHandlers: new Security(),
-    prefix: "v1",
-  };
-  
-
-const server = fastify({
-    logger: true,
-})
-
-server.register(fastifyOpenapiGlue, options)
-
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-}) 
+buildApp().then(server => {
+    server.listen({ port: 8080 }, (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log(`Server listening at ${address}`);
+    });
+});
