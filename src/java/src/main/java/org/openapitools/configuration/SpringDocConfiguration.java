417113f31cba2a124f0a175a3f99b98b14ded585
Davide Mendolia <davide@gokarumi.com>
2025-05-24 11:49:17 +0200

    spotless format

--- src/java/src/main/java/org/openapitools/configuration/SpringDocConfiguration.java
+++ src/java/src/main/java/org/openapitools/configuration/SpringDocConfiguration.java
@@ -1,27 +1,20 @@
 package org.openapitools.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfiguration {
 
-    @Bean(name = "org.openapitools.configuration.SpringDocConfiguration.apiInfo")
-    OpenAPI apiInfo() {
-        return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("Lamp Control API")
-                                .description("A simple API for controlling lamps, demonstrating CRUD operations. ")
-                                .version("1.0.0")
-                )
-        ;
-    }
+  @Bean(name = "org.openapitools.configuration.SpringDocConfiguration.apiInfo")
+  OpenAPI apiInfo() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Lamp Control API")
+                .description("A simple API for controlling lamps, demonstrating CRUD operations. ")
+                .version("1.0.0"));
+  }
 }
