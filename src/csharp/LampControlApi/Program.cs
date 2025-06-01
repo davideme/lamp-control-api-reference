e6a3a504638dccacbe8c8fb27a3fdfc7beef81ac
Davide Mendolia <davide@gokarumi.com>
2025-06-01 09:28:09 +0000

    Add configuration files and implement code style rules for C# project

--- src/csharp/LampControlApi/Program.cs
+++ src/csharp/LampControlApi/Program.cs
@@ -23,13 +23,11 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast(
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+            summaries[Random.Shared.Next(summaries.Length)]))
         .ToArray();
     return forecast;
 })
@@ -38,7 +36,7 @@ app.MapGet("/weatherforecast", () =>
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+sealed record WeatherForecast(DateOnly date, int temperatureC, string? summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(temperatureC / 0.5556);
 }
