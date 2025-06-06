1bc6ad90e717d1fe0b71a242ec45e464b3602ea5
Davide Mendolia <davide@gokarumi.com>
2025-06-06 18:31:01 +0000

    refactor: consolidate E2E tests by removing redundant test class and updating imports

--- src/csharp/LampControlApi/Tests/LampApiE2ETests.cs
+++ src/csharp/LampControlApi/Tests/LampApiE2ETests.cs
@@ -1,5 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Threading.Tasks;
+using LampControlApi.Controllers;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
