e2b3ab2d7c50276d7a7d6410a7101b1288847955
Davide Mendolia <davide@gokarumi.com>
2025-06-08 17:51:17 +0000

    Add jsonSerialize return type attribute and implement comprehensive tests for LampRepository

--- src/php/lamp-control-api/lib/BaseModel.php
+++ src/php/lamp-control-api/lib/BaseModel.php
@@ -291,6 +291,7 @@ SCHEMA;
      *
      * @return mixed Returns data which can be serialized by json_encode(), which is a value of any type other than a resource.
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->getData();
