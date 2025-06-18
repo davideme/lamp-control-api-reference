b24050f926731b1abd01d9d000ffd2af10172437
Davide Mendolia <davide@gokarumi.com>
2025-06-18 20:37:28 +0000

    feat: Implement lamp control API with OpenAPI integration
    - Add OpenAPI configuration file (cfg.yaml) for lamp API.
    - Generate API handler code (lamp.gen.go) using oapi-codegen.
    - Create lamp API implementation (lamp.go) with unimplemented methods.
    - Update go.mod to include necessary dependencies for OpenAPI and chi router.
    - Implement main server logic in main.go to serve the lamp API.
    - Add swagger specification handling for API documentation.

---
+++ src/go/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/davideme/lamp-control-api-reference/api"
+	"github.com/go-chi/chi/v5"
+	middleware "github.com/oapi-codegen/nethttp-middleware"
+)
+
+func main() {
+	port := flag.String("port", "8080", "Port for test HTTP server")
+	flag.Parse()
+
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+		os.Exit(1)
+	}
+
+	// Clear out the servers array in the swagger spec, that skips validating
+	// that server names match. We don't know how this thing will be run.
+	swagger.Servers = nil
+
+	// Create an instance of our handler which satisfies the generated interface
+	lamp := api.NewStrictHandler(api.NewLampAPI(), nil)
+
+	// This is how you set up a basic chi router
+	r := chi.NewRouter()
+
+	// Use our validation middleware to check all requests against the
+	// OpenAPI schema.
+	r.Use(middleware.OapiRequestValidator(swagger))
+
+	// We now register our lamp above as the handler for the interface
+	api.HandlerFromMux(lamp, r)
+
+	s := &http.Server{
+		Handler: r,
+		Addr:    net.JoinHostPort("0.0.0.0", *port),
+	}
+
+	// And we serve HTTP until the world ends.
+	log.Fatal(s.ListenAndServe())
+}
