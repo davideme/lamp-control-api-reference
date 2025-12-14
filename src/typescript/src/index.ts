72b29272620777203869f06b4a08357c8ab03498
Davide Mendolia <davide@gokarumi.com>
2025-12-14 17:36:57 +0100

    Update index.ts

--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -1,8 +1,11 @@
 import { buildApp } from './infrastructure/app.ts';
 import type { FastifyInstance } from 'fastify';
 
+const PORT = parseInt(process.env.PORT || '8080', 10);
+const HOST = process.env.HOST || '0.0.0.0';
+
 buildApp().then((server: FastifyInstance) => {
-  server.listen({ port: 8080 }, (err: Error | null, address: string) => {
+  server.listen({ port: PORT, host: HOST }, (err: Error | null, address: string) => {
     if (err) {
       server.log.error(err);
       process.exit(1);
