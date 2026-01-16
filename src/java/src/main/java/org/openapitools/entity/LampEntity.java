7aef3acec3f23ef4c5533a253aa8ac9ec6de9608
Davide Mendolia <davide@gokarumi.com>
2026-01-16 11:01:05 +0100

    Add auto-generation strategy to LampEntity id
    Annotated the id field in LampEntity with @GeneratedValue using GenerationType.AUTO to enable automatic generation of primary key values.

--- src/java/src/main/java/org/openapitools/entity/LampEntity.java
+++ src/java/src/main/java/org/openapitools/entity/LampEntity.java
@@ -2,6 +2,8 @@ package org.openapitools.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
@@ -24,6 +26,7 @@ import org.hibernate.annotations.Where;
 public class LampEntity {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "id", nullable = false, updatable = false)
   private UUID id;
 
